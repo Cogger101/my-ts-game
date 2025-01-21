7735c38045eff0c793f8c7b762793bb433ca2a59
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-01-21 21:00:56 +0000

    adding word display placeholders and function to match keyboard btns to letters in word display

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import "./styles/style.scss";
 import { alphabet, animals, cities, countries } from "./data";
 
 const keyboard = document.querySelector<HTMLDivElement>(".keyboard");
+const wordDisplayContainer =
+    document.querySelector<HTMLUListElement>(".word-display");
 
-if (!keyboard) {
-    throw new Error("Can't find keyboard div element");
+if (!keyboard || !wordDisplayContainer) {
+    throw new Error(
+        "Can't find div for keyboard or UL for word display container"
+    );
 }
+
+// Generate keyboard buttons
 
 alphabet.forEach((letter) => {
     const keyboardLetterBtn = document.createElement("button");
@@ -14,6 +20,47 @@ alphabet.forEach((letter) => {
 
     keyboardLetterBtn.addEventListener("click", () => {
         console.log(`${letter} button was clicked on the keyboard`);
+        handleLetterClicks(letter);
     });
     keyboard.appendChild(keyboardLetterBtn);
 });
+
+// Selecting a random word from data (animals array to start with)
+
+const targetWord = animals[Math.floor(Math.random() * animals.length)]
+    .toUpperCase()
+    .split("");
+console.log(targetWord);
+const guessedLetters: string[] = [];
+
+// Create work placeholders
+
+wordDisplayContainer.innerHTML = "";
+targetWord.forEach(() => {
+    const li = document.createElement("li");
+    li.className = "word-display__letter";
+    li.textContent = "_";
+    wordDisplayContainer.appendChild(li);
+});
+
+const wordDisplay = wordDisplayContainer.querySelectorAll<HTMLLIElement>(
+    ".word-display__letter"
+);
+
+// Match/handle the keyboard and word display inputs
+
+const handleLetterClicks = (key: string) => {
+    key = key.toUpperCase();
+    if (/^[A-Z]$/.test(key) && !guessedLetters.includes(key)) {
+        guessedLetters.push(key);
+        if (targetWord.includes(key)) {
+            targetWord.forEach((letter, index) => {
+                if (letter === key) {
+                    wordDisplay[index].textContent = key;
+                }
+            });
+        } else {
+            console.log(`${key} is not in the word.`);
+        }
+    }
+};
