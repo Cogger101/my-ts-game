566d4d7b99e8519c0b78f1f51b8492e8db903eb1
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-01-26 16:13:51 +0000

    adding the hangman drawing function to handle incorrect guesses and body part to be drawn

---
+++ src/hangmanDrawing.ts
@@ -0,0 +1,22 @@
+const hangmanDrawingContainer =
+    document.querySelector<HTMLDivElement>("hangman-drawing");
+
+if (!hangmanDrawingContainer) {
+    throw new Error("Can't find hangman drawing container");
+}
+export const hangmanDrawing = (incorrectLetterCount: number) => {
+    const hangmanParts = [
+        ".hangman-drawing__head",
+        ".hangman-drawing__body",
+        ".hangman-drawing__left-arm",
+        ".hangman-drawing__right-arm",
+        ".hangman-drawing__left-leg",
+        ".hangman-drawing__right-leg",
+    ];
+    hangmanParts.forEach((selected, index) => {
+        const part = document.querySelector<HTMLDivElement>(selected);
+        if (part) {
+            part.classList.toggle("visible", index < incorrectLetterCount);
+        }
+    });
+};
